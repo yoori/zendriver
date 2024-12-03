cf7adc7cf53da8ce568df02182199d5cf64addac
yoori <yuri.kuznecov@gmail.com>
2024-12-03 17:37:52 +0000

    x

--- zendriver_flare_bypasser/core/connection.py
+++ zendriver_flare_bypasser/core/connection.py
@@ -431,6 +431,7 @@ class Connection(metaclass=CantTouchThis):
             self.mapper.update({tx.id: tx})
             if not _is_update:
                 await self._register_handlers()
+            logger.debug("send message %s (message_id:%d): %s", tx, tx.id, str(message))
             await self.websocket.send(tx.message)
             try:
                 return await tx
